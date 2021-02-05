a61d6a23a9c0a48b4224e97060f4ba6e80c41e97
Joe Wu <wuzongsheng@gmail.com>
2021-02-05 09:50:05 +0000

    Handle recv 0

--- source/lib/socket.cpp
+++ source/lib/socket.cpp
@@ -28,18 +28,19 @@ namespace miu::net {
 //}
 
 socket::socket(int32_t raw)
-    : _raw(raw) {}
+    : _raw(raw) {
+}
 
 socket::socket(int32_t family, int32_t type)
-    : _raw(::socket(family, type, 0)) {}
+    : _raw(::socket(family, type, 0)) {
+}
 
 socket::socket(socket&& another)
     : _raw(another._raw) {
     another._raw = -1;
 }
 
-socket&
-socket::operator=(socket&& another) {
+socket& socket::operator=(socket&& another) {
     std::swap(_raw, another._raw);
     return *this;
 }
@@ -49,75 +50,61 @@ socket::~socket() {
     ::close(_raw);
 }
 
-int32_t
-socket::type() const {
+int32_t socket::type() const {
     return getsockopt<int32_t>(_raw, SO_TYPE);
 }
 
-bool
-socket::nodelay() const {
+bool socket::nodelay() const {
     int32_t val;
     socklen_t len = sizeof(val);
     ::getsockopt(_raw, IPPROTO_TCP, TCP_NODELAY, (char*)&val, &len);
     return val != 0;
 }
-void
-socket::set_nodelay(bool v) {
+void socket::set_nodelay(bool v) {
     auto val = v ? 1 : 0;
     ::setsockopt(_raw, IPPROTO_TCP, TCP_NODELAY, (const char*)&val, sizeof(val));
 }
 
-int32_t
-socket::sndbuf() const {
+int32_t socket::sndbuf() const {
     return getsockopt<int32_t>(_raw, SO_SNDBUF);
 }
-void
-socket::set_sndbuf(int32_t v) {
+void socket::set_sndbuf(int32_t v) {
     setsockopt(_raw, SO_SNDBUF, v);
 }
 
-int32_t
-socket::rcvbuf() const {
+int32_t socket::rcvbuf() const {
     return getsockopt<int32_t>(_raw, SO_RCVBUF);
 }
-void
-socket::set_rcvbuf(int32_t v) {
+void socket::set_rcvbuf(int32_t v) {
     setsockopt(_raw, SO_RCVBUF, v);
 }
 
-bool
-socket::reuseaddr() const {
+bool socket::reuseaddr() const {
     return getsockopt<int32_t>(_raw, SO_REUSEADDR) != 0;
 }
-void
-socket::set_reuseaddr(bool v) {
+void socket::set_reuseaddr(bool v) {
     setsockopt(_raw, SO_REUSEADDR, v ? 1 : 0);
 }
 
-bool
-socket::acceptconn() const {
+bool socket::acceptconn() const {
     return getsockopt<int32_t>(_raw, SO_ACCEPTCONN) != 0;
 }
 
-com::microseconds
-socket::timeout() const {
+com::microseconds socket::timeout() const {
     auto val = getsockopt<struct timeval>(_raw, SO_RCVTIMEO);
     return { val.tv_sec * 1000000 + val.tv_usec };
 }
 
-void
-socket::set_timeout(com::microseconds val) {
-    time_t sec = val.count() / 1000000;
+void socket::set_timeout(com::microseconds val) {
+    time_t sec       = val.count() / 1000000;
     suseconds_t usec = val.count() % 1000000;
     setsockopt(_raw, SO_RCVTIMEO, timeval { sec, usec });
 }
 
-bool
-socket::nonblock() const {
+bool socket::nonblock() const {
     return (::fcntl(_raw, F_GETFL) & O_NONBLOCK) != 0;
 }
-void
-socket::set_nonblock(bool v) {
+void socket::set_nonblock(bool v) {
     auto flags = ::fcntl(_raw, F_GETFL);
     if (v) {
         flags |= O_NONBLOCK;
@@ -127,8 +114,7 @@ socket::set_nonblock(bool v) {
     ::fcntl(_raw, F_SETFL, flags);
 }
 
-bool
-socket::bind(address const& addr) {
+bool socket::bind(address const& addr) {
     auto err = ::bind(_raw, addr.raw(), addr.len());
     if (err) {
         log::error(+"socket bind", addr.str(), errno, (const char*)strerror(errno));
@@ -136,8 +122,7 @@ socket::bind(address const& addr) {
     return !err;
 }
 
-bool
-socket::listen() {
+bool socket::listen() {
     auto err = ::listen(_raw, 1);
     if (err) {
         log::error(+"socket listen", errno, (const char*)strerror(errno));
@@ -145,20 +130,20 @@ socket::listen() {
     return !err;
 }
 
-socket
-socket::accept() {
+socket socket::accept() {
     sockaddr_in addr {};
     uint32_t addrlen = sizeof(addr);
-    auto cli = ::accept(_raw, (sockaddr*)&addr, &addrlen);
+    auto cli         = ::accept(_raw, (sockaddr*)&addr, &addrlen);
     if (cli < 0) {
-        log::error(+"socket accept", errno, (const char*)strerror(errno));
+        if (errno != EAGAIN) {
+            log::error(+"socket accept", errno, (const char*)strerror(errno));
+        }
     }
 
     return { cli };
 }
 
-bool
-socket::connect(address const& addr) {
+bool socket::connect(address const& addr) {
     if (::connect(_raw, addr.raw(), addr.len())) {
         log::error(+"socket connect", errno, (const char*)strerror(errno));
         return false;
@@ -167,8 +152,7 @@ socket::connect(address const& addr) {
     return true;
 }
 
-int32_t
-socket::send(const void* data, uint32_t size) {
+int32_t socket::send(const void* data, uint32_t size) {
     auto sent = ::send(_raw, data, size, MSG_NOSIGNAL);
     if (sent < 0) {
         log::error(+"socket send", errno, (const char*)strerror(errno));
@@ -176,11 +160,14 @@ socket::send(const void* data, uint32_t size) {
     return sent;
 }
 
-int32_t
-socket::recv(void* buf, uint32_t len) {
+int32_t socket::recv(void* buf, uint32_t len) {
     auto rcvd = ::recvfrom(_raw, buf, len, MSG_NOSIGNAL, nullptr, nullptr);
-    if (rcvd < 0) {
-        log::error(+"socket recv", errno, (const char*)strerror(errno));
+    if (UNLIKELY(rcvd < 0)) {
+        if (errno != EAGAIN) {
+            log::error(+"socket recv", errno, (const char*)strerror(errno));
+        } else {
+            rcvd = 0;    // no data received ..
+        }
     }
     return rcvd;
 }
--- unittest/ut_udsock.cpp
+++ unittest/ut_udsock.cpp
@@ -1,14 +1,26 @@
 #include <gtest/gtest.h>
 
 #include <future>
+#include <log/log.hpp>
 #include <thread>
 
 #include "net/udsock.hpp"
 #include "source/lib/udaddr.hpp"
 
+using namespace std::chrono_literals;
 using miu::net::udsock;
 
-TEST(ut_udsock, server) {
+struct ut_udsock : public testing::Test {
+    void SetUp() override {
+        using miu::log::severity;
+        // miu::log::log::instance()->reset(severity::DEBUG, 1024);
+    }
+    void TearDown() override {
+        // miu::log::log::instance()->dump();
+    }
+};
+
+TEST_F(ut_udsock, server) {
     auto sock = udsock::create_server("ut_udsock");
     EXPECT_TRUE(sock);
     EXPECT_EQ(SOCK_STREAM, sock.type());
@@ -24,18 +36,18 @@ TEST(ut_udsock, server) {
     EXPECT_TRUE(udsock::create_server("ut_udsock"));
 }
 
-TEST(ut_udsock, timeout) {
+TEST_F(ut_udsock, accept_timeout) {
     auto sock = udsock::create_server("ut_udsock");
 
     EXPECT_EQ(miu::com::microseconds::zero(), sock.timeout());
 
-    sock.set_timeout(miu::com::microseconds(12000));
+    sock.set_timeout(12000us);
     EXPECT_EQ(12000, sock.timeout().count());
 
     EXPECT_FALSE(sock.accept());    // should timeout
 }
 
-TEST(ut_udsock, client) {
+TEST_F(ut_udsock, client) {
     // failed to connect server
     EXPECT_FALSE(udsock::create_client("ut_udsock"));
 
@@ -63,7 +75,7 @@ TEST(ut_udsock, client) {
     thrd.join();
 }
 
-TEST(ut_udsock, recv_failed) {
+TEST_F(ut_udsock, recv_failed) {
     std::promise<bool> ready;
     std::thread thrd([&]() {
         auto sock = udsock::create_server("ut_udsock");
@@ -79,7 +91,34 @@ TEST(ut_udsock, recv_failed) {
     EXPECT_GE(0, sock.recv(buf, sizeof(buf)));
 }
 
-TEST(ut_udsock, send_failed) {
+TEST_F(ut_udsock, recv_timeout) {
+    std::promise<bool> ready;
+    std::promise<bool> done;
+    std::thread thrd([&]() {
+        auto sock = udsock::create_server("ut_udsock");
+
+        ready.set_value(true);
+        auto cli_sock = sock.accept();
+        cli_sock.set_timeout(100ms);
+
+        char buf[8];
+        EXPECT_EQ(0, cli_sock.recv(buf, sizeof(buf)));
+
+        done.get_future().get();
+    });
+    ready.get_future().get();    // waiting for server get ready for accept
+
+    auto sock = udsock::create_client("ut_udsock");
+    sock.set_timeout(100ms);
+
+    char buf[8] {};
+    EXPECT_EQ(0, sock.recv(buf, sizeof(buf)));
+
+    done.set_value(true);
+    thrd.join();
+}
+
+TEST_F(ut_udsock, send_failed) {
     std::promise<bool> ready;
     std::thread thrd([&]() {
         auto sock = udsock::create_server("ut_udsock");
